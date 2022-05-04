72b8e5cc1b4ace5c7f0f2193b17ce446563583aa
SalifuSalu <brainstormbright@gmail.com>
2022-05-04 21:51:25 +0000

    changed 24 hour time to 12 hour time
    some time format changes made

--- clock.py
+++ clock.py
@@ -12,18 +12,18 @@ root.resizable(False, False)  #window should not be resizable
 
 #function to produce and update time
 def timer():
-    currentTime = time.strftime("%H:%M:%S")
+    currentTime = time.strftime("%I:%M:%S")
     clock.configure(text=currentTime)
     clock.after(200, timer)
 
 
 #other GUI for the screen of the clock
 header = Label(root, text="M E T R O  D I G I T A L  C L O C K", bg="#1a52ed", fg="white", font=("times", 24, "bold"))
-header.pack(expand=NO, fill=X, anchor="center", side=TOP, ipady=10)
+header.pack(expand=NO, fill=X, anchor="center", side=TOP, ipady=11)
 
 clock = Label(root, bg="white", fg="black", font=("times", 20, "bold"))
 clock.pack(anchor="center", fill=NONE, expand=NO, padx=20, pady=(20, 5), ipadx=90, ipady=30)
-clock_legend = Label(text="Hours   Minutes   Seconds", bg="grey", fg="yellow", font=("times", 12, "bold"))
+clock_legend = Label(text="   Hours||Minutes||Seconds", bg="grey", fg="yellow", font=("times", 12, "bold"))
 clock_legend.pack(pady=(3, 0), padx=(10, 10), anchor="center")
 timer()
 
